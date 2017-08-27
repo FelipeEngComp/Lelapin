2a7fec6fca8e1f75aff7421b9640c124821454e9
Felipe Ribeiro Rosa Alves <feliperibeiroalves@gmail.com>
2017-08-26 22:17:15 -0300

    Add files via upload

---
+++ sistemadecadastro/SistemaDeCadastro.java
@@ -0,0 +1,21 @@
+
+package sistemadecadastro;
+
+import Visao.Lelapin;
+
+//provavel trabalho de POO
+//Desenvolvedor : Felipe Ribeiro Rosa Alves
+public class SistemaDeCadastro {
+
+   
+    public static void main(String[] args) {
+        
+        Lelapin TelaPrincipal = new Lelapin();
+        
+        TelaPrincipal.setVisible(true);
+        
+        
+        
+    }
+    
+}
